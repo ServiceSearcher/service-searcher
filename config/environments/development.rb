0955c4dd940bfbb8b7d19dc861f614b1aa7fde60
mjprude <michaelprude@gmail.com>
2015-02-10 19:22:41 -0500

    adds gmail smtp to dev and production environments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,4 +37,14 @@ Rails.application.configure do
 
   #specify mailer url for devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "bxd.herokuapp.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
 end
